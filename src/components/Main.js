6adf98ff4dbbac0b5d151b64a28f3c6ea8601334
A-rielox <arielox.ag@gmail.com>
2023-04-04 12:21:09 -0600

    a comer

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,56 +10,56 @@ import Bottom from "./mainComponents/Bottom";
 import IntroOverlay from "./mainComponents/IntroOverlay";
 
 const Main = ({ loaded }) => {
-	// --------------- para ContactModal
-	const [isContactModal, setIsContactModal] = useState(false);
-	const contactModalToggler = () => {
-		setIsContactModal(!isContactModal);
-	};
-	// --------------- Fin ContactModal
+   // --------------- para ContactModal
+   const [isContactModal, setIsContactModal] = useState(false);
+   const contactModalToggler = () => {
+      setIsContactModal(!isContactModal);
+   };
+   // --------------- Fin ContactModal
 
-	return (
-		<MainContainer>
-			<Container>
-				<IntroOverlay loaded={loaded} />
+   return (
+      <MainContainer>
+         <Container>
+            <IntroOverlay loaded={loaded} />
 
-				<Top loaded={loaded} contactModalToggler={contactModalToggler} />
+            <Top loaded={loaded} contactModalToggler={contactModalToggler} />
 
-				<Bottom loaded={loaded} />
-			</Container>
+            <Bottom loaded={loaded} />
+         </Container>
 
-			{/* CONTACT-MODAL */}
-			<AnimatePresence>
-				{isContactModal && (
-					<ContactModal
-						isContactModal={isContactModal}
-						contactModalToggler={contactModalToggler}
-					/>
-				)}
-			</AnimatePresence>
-		</MainContainer>
-	);
+         {/* CONTACT-MODAL */}
+         <AnimatePresence>
+            {isContactModal && (
+               <ContactModal
+                  isContactModal={isContactModal}
+                  contactModalToggler={contactModalToggler}
+               />
+            )}
+         </AnimatePresence>
+      </MainContainer>
+   );
 };
 
 export default Main;
 
 const MainContainer = styled.div`
-	background: ${(props) => props.theme.body};
-	width: 100vw;
-	height: 100vh;
-	/* overflow: hidden; */
+   background: ${(props) => props.theme.body};
+   width: 100vw;
+   height: 100vh;
+   /* overflow: hidden; */
 
-	position: relative;
+   position: relative;
 
-	h2,
-	h3,
-	h4,
-	h5,
-	h6 {
-		font-family: "Karla", sans-serif;
-		font-weight: 500;
-	}
+   h2,
+   h3,
+   h4,
+   h5,
+   h6 {
+      font-family: "Karla", sans-serif;
+      font-weight: 500;
+   }
 `;
 
 const Container = styled.div`
-	/* padding: 2rem; */
+   /* padding: 2rem; */
 `;
